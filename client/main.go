adab7b671225dcec8327de84d193bd8325a63c45
tooruse <85640573+tooruse@users.noreply.github.com>
2021-06-09 12:40:04 -0700

    Create main.go

---
+++ client/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+)
+
+
+func main(){
+  p := 8000
+  connect(p)
+}
+
+
+func connect(){
+  for {
+        // do something
+    if !condition {      // the condition stops matching
+          //send init data to server
+          break        // break out of the loop
+    }
+  }
+}
